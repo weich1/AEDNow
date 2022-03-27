f743c90ed7c659c9b20cbd7e7c5576d7b3593919
Draper, Daniel <ddraper@dmacc.edu>
2022-03-27 14:17:04 -0500

    Added POJO object Added test JSON file

---
+++ app/src/main/java/com/miscrew/aednow/MapData.java
@@ -0,0 +1,16 @@
+package com.miscrew.aednow;
+
+public class MapData {
+    private String title;
+    private long lat;
+    private long lng;
+    private String description;
+
+    public MapData(String title, long lat, long lng, String description) {
+        this.title = title;
+        this.lat = lat;
+        this.lng = lng;
+        this.description = description;
+    }
+
+}
--- app/src/main/java/com/miscrew/aednow/MapsActivity.java
+++ app/src/main/java/com/miscrew/aednow/MapsActivity.java
@@ -1,7 +1,9 @@
 package com.miscrew.aednow;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.FragmentActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,13 +12,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.*;
 import com.miscrew.aednow.databinding.ActivityMapsBinding;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
-
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +32,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // create Gson instance
+        Gson gson = new Gson();
+        try {
+
+            // create a reader
+            Reader reader = Files.newBufferedReader(Paths.get("main/assets/map.json"));
+            // convert JSON file to map
+            Map<?, ?> map = gson.fromJson(reader, Map.class);
+
+            // print map entries
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + "=" + entry.getValue());
+            }
+
+            // close reader
+            reader.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //String js = gson.fromJson()
     }
+
+
 
     /**
      * Manipulates the map once available.
@@ -39,6 +72,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -48,4 +83,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Grandview University"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
 }
